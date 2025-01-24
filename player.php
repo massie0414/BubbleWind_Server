07f911dda0bb1466b8970655b110f627fc52ec7a
massie0414 <massie0414@gmail.com>
2025-01-25 02:57:48 +0900

    簡単なプレイヤーデータのやり取りを作成

--- player.php
+++ player.php
@@ -1,8 +1,8 @@
 <?php
 $id = 1;
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=test;charset=utf8mb4',
-    'testuser',
+    'mysql:host=localhost;dbname=BubbleWind;charset=utf8mb4',
+    'ggj',
     'password'
     );
 
@@ -15,7 +15,7 @@ $json_data = [];
 
 // $stmt = $pdo->prepare("SELECT * FROM test_data WHERE id = :id");
 // $stmt->bindParam(':id', $id);
-$stmt = $pdo->prepare("SELECT * FROM test_data");
+$stmt = $pdo->prepare("SELECT user_id, user_name, x, y, z FROM player_data");
 $res = $stmt->execute();
 if( $res ) {
 //	$data = $stmt->fetch();
@@ -27,8 +27,11 @@ if( $res ) {
         // $json_data["data"] = $data[1];
 
         $json_data[] = [
-            "id" => (int)$data[0],
-            "data" => $data[1]
+            "user_id" => (int)$data[0],
+            "user_name" => $data[1],
+            "x" => (float)$data[2],
+            "y" => (float)$data[3],
+            "z" => (float)$data[4],
         ];
 
     }
@@ -37,9 +40,9 @@ $pdo = null;
 
 // 全体の構造を作成
 $json_data = [
-    "mysqldata" => $json_data
+    "player_data" => $json_data
 ];
 
-//sleep(5);
+sleep(5);
 
 echo json_encode($json_data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
